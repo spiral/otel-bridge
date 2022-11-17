f01ef399199ceb28226c9d91759141c44a860507
kastahov <kastahov1021@gmail.com>
2022-11-17 18:24:56 +0200

    Fix psalm

--- src/Trace/DeferredSpanExporter.php
+++ src/Trace/DeferredSpanExporter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Spiral\OpenTelemetry\Trace;
 
 use OpenTelemetry\SDK\Common\Future\CancellationInterface;
+use OpenTelemetry\SDK\Common\Future\CompletedFuture;
 use OpenTelemetry\SDK\Common\Future\FutureInterface;
 use OpenTelemetry\SDK\Trace\ExporterFactory;
 use OpenTelemetry\SDK\Trace\SpanExporterInterface;
@@ -23,22 +24,29 @@ final class DeferredSpanExporter implements SpanExporterInterface
         throw new \Exception('Method is not supported.');
     }
 
+    /**
+     * @psalm-suppress InvalidReturnType,InvalidReturnStatement
+     */
     public function export(iterable $spans, ?CancellationInterface $cancellation = null): FutureInterface
     {
-        return $this->getExporter()->export($spans, $cancellation);
+        if ($this->getExporter() !== null) {
+            return $this->getExporter()->export($spans, $cancellation);
+        }
+
+        return new CompletedFuture(SpanExporterInterface::STATUS_FAILED_NOT_RETRYABLE);
     }
 
     public function shutdown(?CancellationInterface $cancellation = null): bool
     {
-        return $this->getExporter()->shutdown($cancellation);
+        return (bool) $this->getExporter()?->shutdown($cancellation);
     }
 
     public function forceFlush(?CancellationInterface $cancellation = null): bool
     {
-        return $this->getExporter()->forceFlush($cancellation);
+        return (bool) $this->getExporter()?->forceFlush($cancellation);
     }
 
-    private function getExporter(): SpanExporterInterface
+    private function getExporter(): ?SpanExporterInterface
     {
         if ($this->exporter === null) {
             $this->exporter = $this->exporterFactory->fromEnvironment();
