d4cec62292060a23cb623e7c9b4504aec15620ce
roxblnfk <roxblnfk@ya.ru>
2025-01-23 15:11:28 +0400

    Add tests

--- tests/src/TracerTest.php
+++ tests/src/TracerTest.php
@@ -20,11 +20,11 @@ class TracerTest extends TestCase
         $tracer = new Tracer(
             new Container(),
             $tracerMockery = m::mock(TracerInterface::class),
-            m::mock(TextMapPropagatorInterface::class)
+            m::mock(TextMapPropagatorInterface::class),
         );
 
         $tracerMockery->shouldReceive('spanBuilder')->once()->with('test')->andReturn(
-            $spanBuilder = m::mock(SpanBuilderInterface::class)
+            $spanBuilder = m::mock(SpanBuilderInterface::class),
         );
 
         $spanBuilder->shouldReceive('setStartTimestamp')->with(9999)->andReturnSelf();
@@ -32,17 +32,42 @@ class TracerTest extends TestCase
         $spanBuilder->shouldReceive('startSpan')->once()->andReturn($span = m::mock(SpanInterface::class));
 
         $span->shouldReceive('updateName')->once()->with('test')->andReturnSelf();
-        $span->shouldReceive('setAttributes')->with(['baz' => 'baf', 'foo' => 'bar'])->andReturnSelf();
+        $span->shouldReceive('setAttributes')->with([
+            'baz' => 'baf',
+            'foo' => 'bar',
+            'list' => ['foo', 'baf', 42],
+            'kv' => ['key' => 'value', 'foo' => 'bar'],
+            'nested' => ['key' => '{"foo":"bar"}'],
+            'list-of-one' => 42,
+            'stringable' => 'stringable value',
+            'jsonable' => '{"foo":{"bar":"baz"}}',
+        ])->andReturnSelf();
         $span->shouldReceive('end')->once()->withNoArgs()->andReturnSelf();
 
         $result = $tracer->trace(
             'test',
             callback: static function (\Spiral\Telemetry\SpanInterface $span) {
                 $span->setAttribute('baz', 'baf');
+                $span->setAttribute('list', ['foo', 'baf', 42]);
+                $span->setAttribute('kv', ['key' => 'value', 'foo' => 'bar']);
+                $span->setAttribute('nested', ['key' => ['foo' => 'bar']]);
+                $span->setAttribute('list-of-one', [42]);
+                $span->setAttribute('stringable', new class implements \Stringable {
+                    public function __toString(): string
+                    {
+                        return 'stringable value';
+                    }
+                });
+                $span->setAttribute('jsonable', new class implements \JsonSerializable {
+                    public function jsonSerialize(): array
+                    {
+                        return ['foo' => ['bar' => 'baz']];
+                    }
+                });
                 return 'test';
             },
             attributes: ['foo' => 'bar'],
-            startTime: 9999
+            startTime: 9999,
         );
 
         $this->assertSame('test', $result);
@@ -58,11 +83,11 @@ class TracerTest extends TestCase
         $tracer = new Tracer(
             new Container(),
             $tracerMockery = m::mock(TracerInterface::class),
-            m::mock(TextMapPropagatorInterface::class)
+            m::mock(TextMapPropagatorInterface::class),
         );
 
         $tracerMockery->shouldReceive('spanBuilder')->once()->with('test')->andReturn(
-            $spanBuilder = m::mock(SpanBuilderInterface::class)
+            $spanBuilder = m::mock(SpanBuilderInterface::class),
         );
 
         $spanBuilder->shouldReceive('setStartTimestamp')->with(9999)->andReturnSelf();
@@ -79,7 +104,7 @@ class TracerTest extends TestCase
                 throw $exception;
             },
             attributes: ['foo' => 'bar'],
-            startTime: 9999
+            startTime: 9999,
         );
 
         $this->assertSame('test', $result);
@@ -90,11 +115,11 @@ class TracerTest extends TestCase
         $tracer = new Tracer(
             new Container(),
             $tracerMockery = m::mock(TracerInterface::class),
-            m::mock(TextMapPropagatorInterface::class)
+            m::mock(TextMapPropagatorInterface::class),
         );
 
         $tracerMockery->shouldReceive('spanBuilder')->once()->with('test')->andReturn(
-            $spanBuilder = m::mock(SpanBuilderInterface::class)
+            $spanBuilder = m::mock(SpanBuilderInterface::class),
         );
 
         $spanBuilder->shouldReceive('setStartTimestamp')->with(9999)->andReturnSelf();
@@ -116,7 +141,7 @@ class TracerTest extends TestCase
             },
             attributes: ['foo' => 'bar'],
             scoped: true,
-            startTime: 9999
+            startTime: 9999,
         );
 
         $this->assertSame('test', $result);
