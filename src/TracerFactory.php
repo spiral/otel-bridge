e45f309a367eae653d6ee6cf683c04e0f1917843
Rauan Mayemir <rauan@mayemir.io>
2025-02-16 15:17:30 +0500

    Fixes a bug with array_intersect_ukey
    This fixes a bug where user-defined compare func should have been a proper string comparison func. Always returning -1 breaks the intersect function.

--- src/TracerFactory.php
+++ src/TracerFactory.php
@@ -23,7 +23,13 @@ final class TracerFactory implements TracerFactoryInterface
         $context = \array_intersect_ukey(
             $context,
             \array_flip($this->propagator->fields()),
-            static fn(string $key1, string $key2): int => (\strtolower($key1) === \strtolower($key2)) ? 0 : -1,
+            static function(string $key1, string $key2): int {
+                if (\strtolower($key1) === \strtolower($key2)) {
+                    return 0;
+                }
+
+                return \strtolower($key1) > \strtolower($key2) ? 1 : -1;
+            },
         );
 
         return new Tracer($this->scope, $this->tracer, $this->propagator, $context);
