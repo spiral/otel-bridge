13d8346efe399930e5b2fcf60f7f9c865efc8e1e
Rauan Mayemir <rauan@mayemir.io>
2025-02-17 17:07:36 +0500

    Add tracer factory tests to check for context propagation

--- tests/src/TracerFactoryTest.php
+++ tests/src/TracerFactoryTest.php
@@ -25,4 +25,28 @@ class TracerFactoryTest extends TestCase
 
         $this->assertInstanceOf(TelemetryTracerInterface::class, $factory->make(['foo' => 'bar']));
     }
+
+    public function testMakePropagatesContext(): void
+    {
+        $factory = new TracerFactory(
+            m::mock(ScopeInterface::class),
+            m::mock(TracerInterface::class),
+            $propagator = m::mock(TextMapPropagatorInterface::class)
+        );
+
+        $propagator->shouldReceive('fields')->withNoArgs()->andReturn(['foo3', 'foo4', 'foo5']);
+
+        $tracer = $factory->make([
+            'foo1' => 'bar1',
+            'foo2' => 'bar2',
+            'foo3' => 'bar3',
+            'foo4' => 'bar4',
+            'foo5' => 'bar5',
+            'foo6' => 'bar6',
+        ]);
+
+        $propagated = ['foo3' => 'bar3', 'foo4' => 'bar4', 'foo5' => 'bar5'];
+
+        $this->assertEquals($propagated, $tracer->getContext());
+    }
 }
